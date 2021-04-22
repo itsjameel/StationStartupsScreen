f993835959aaf33cde88b9fc2d2daed044d85c7d
Jameel Amir <me@jameel.xyz>
2021-04-22 19:30:14 +0300

    added new stuff

--- main.py
+++ main.py
@@ -5,30 +5,37 @@ from PIL import ImageDraw
 import requests
 fontFile = "./helveticaneuelt-arabic-55-roman.ttf"
 imageFile = "/home/jameel/Coding/StationStartupsScreen/StationScreenTest-35.jpg"
-textSize = 150
-font = ImageFont.truetype(fontFile, textSize)
+textSize = 170
+infoSize = 150
+descriptionFont = ImageFont.truetype(fontFile, textSize)
+infoFont = ImageFont.truetype(fontFile, infoSize)
 image = Image.open(imageFile)
-myRequest = requests.get("https://admin.jameel.xyz/stationstartups").json()[22]
+myRequest = requests.get("https://admin.jameel.xyz/stationstartups").json()[15]
 requestText = myRequest['descriptionar']
 requestLogo = Image.open(requests.get(myRequest['logo'], stream=True).raw)
 requestPhone = myRequest['phone']
 requestEmail = myRequest['email']
 descriptionText = requestText
-descriptionWidth = 70
+print(len(descriptionText))
+descriptionWidth = 60
 descriptionWrapped = textwrap.wrap(descriptionText, width=descriptionWidth)
 descriptionWrapped = ' '.join(descriptionWrapped)
 descriptionWrapped = descriptionWrapped.replace("         ", " ")
 descriptionWrapped = textwrap.wrap(descriptionWrapped, width=descriptionWidth)
 descriptionWrapped = '\n'.join(descriptionWrapped)
 draw = ImageDraw.Draw(image)
+print(descriptionWrapped)
+if len(descriptionText) < 300:
+    draw.multiline_text((3200, 2300), descriptionWrapped, font=descriptionFont, direction="rtl", align="right", language="ar")
+elif len(descriptionText) > 300 and len(descriptionText) < 400:
+    draw.multiline_text((3000, 2300), descriptionWrapped, font=descriptionFont, direction="rtl", align="right", language="ar")
+elif len(descriptionText) > 400:
+    draw.multiline_text((3000, 2300), descriptionWrapped, font=descriptionFont, direction="rtl", align="right", language="ar")
+    
 
-draw.multiline_text((2800, 2300), descriptionWrapped, font=font, direction="rtl", align="right")
+draw.text((650, 3200), requestPhone, font=infoFont)
 
-
-draw.text((650, 3200), requestPhone, font=font)
-
-draw.text((650, 3540), requestEmail, font=font)
-
+draw.text((650, 3540), requestEmail, font=infoFont)
 
 basewidth = 1500
 wpercent = (basewidth/float(requestLogo.size[0]))
