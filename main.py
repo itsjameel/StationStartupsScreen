73e3d0078903977e5f066dac717cc27ea3a072e3
Jameel Amir <me@jameel.xyz>
2021-10-06 15:45:13 +0300

    Added stuff

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 import textwrap
 import requests
+import json
+import html2text
+import re
+from Naked.toolshed.shell import execute_js, muterun_js
+
+success = execute_js('index.js')
+
+if success:
+    print("done!")
+else:
+    print("nope")    
+
+def readJSONfile():
+    with open('data.json') as f:
+        data = json.load(f)
+        return data
+
 
 def getStartupData(index):
-    global myRequest
-    myRequest = requests.get("https://admin.jameel.xyz/stationstartups").json()[index]
+    global startup
+    startup = readJSONfile()[index]
+    print(startup)
     global requestText
-    requestText = myRequest['descriptionar']
-    global requestLogo
-    requestLogo = Image.open(requests.get(myRequest['logo'], stream=True).raw)
-    global requestPhone
-    requestPhone = myRequest['phone']
-    global requestEmail
-    requestEmail = myRequest['email']
-    global requestFacebook
-    requestFacebook = myRequest['facebook']
-    global requestInstagram
-    requestInstagram = myRequest['instagram']
+    requestText = html2text.html2text((startup.get('description')))
+    global startupLogo
+    startupLogo = Image.open(requests.get(startup['logo'], stream=True).raw)
+    global startupLocation
+    startupLocation = startup.get('location')
+    global startupPhone
+    startupPhone = startup.get('phone')
+    global startupEmail
+    startupEmail = startup.get('email')
+    global startupFacebook
+    startupFacebook = startup.get('facebook')
+    global startupInstagram
+    startupInstagram = startup.get('instagram')
 
 
-startupQuantity = len(requests.get("https://admin.jameel.xyz/stationstartups").json())
+startupQuantity = len(readJSONfile())
 for i in range(0, startupQuantity):
     from PIL import ImageFont
     from PIL import Image
@@ -33,7 +53,6 @@ for i in range(0, startupQuantity):
     image = Image.open(imageFile)
     draw = ImageDraw.Draw(image)
     getStartupData(i)
-    print(myRequest['name'])
     descriptionText = requestText
     descriptionWidth = 60
     descriptionWrapped = textwrap.wrap(descriptionText, width=descriptionWidth)
@@ -47,6 +66,12 @@ for i in range(0, startupQuantity):
     finalX = 3200 + (7501 - textCoords[-2]) 
 
     draw.multiline_text((finalX, 2300), descriptionWrapped, font=descriptionFont, direction="rtl", align="right", language="ar")
+    if startupLocation == 'Baghdad':
+        draw.rectangle((7000, 730, 7500, 950))
+        draw.text((7100, 650), "بغداد", font=descriptionFont, fill="white")
+    elif startupLocation == 'Mosul':
+        draw.rectangle((7000, 730, 7500, 950))
+        draw.text((7060, 650), "موصل", font=descriptionFont, fill="white")
 
     def resizeImage(image, baseWidth):
         wpercent = (baseWidth/float(image.size[0]))
@@ -63,40 +88,84 @@ for i in range(0, startupQuantity):
     facebookIcon = resizeImage(facebookIcon, 175)
     instagramIcon = resizeImage(instagramIcon, 175)
 
-    if requestPhone:
-        image.paste(phoneIcon, (400, 3180), mask=phoneIcon)
+    infoCoordsStart = 2810
+    global infoCoordsList
+    infoCoordsList = [
+        {"id": "phone", "number": 3180},
+    ]
+    global findInInfoCoordsList
+    def findInInfoCoordsList(id):
+        thing = [item for item in infoCoordsList if item['id'] == id][0]
+        return thing['number']
 
-    if requestEmail:
-        image.paste(emailIcon, (400, 3550), mask=emailIcon)
+    def extractFacebookID(url):
+        x = re.search("^(?:https?:\/\/)?(?:www\.|m\.|touch\.)?(?:facebook\.com|fb(?:\.me|\.com))\/(?!$)(?:(?:\w)*#!\/)?(?:pages\/)?(?:photo\.php\?fbid=)?(?:[\w\-]*\/)*?(?:\/)?(?:profile\.php\?id=)?([^\/?\s]*)(?:\/|&|\?)?.*$", url)
+        if x: 
+            return x.group(1)
+        else:
+            return None
+    
+    def extractInstagramID(url):
+        x = re.search("(?:(?:http|https):\/\/)?(?:www\.)?(?:instagram\.com|instagr\.am)\/([A-Za-z0-9-_\.]+)")
+        if x:
+            return x.group(1)
+        else:
+            return None
 
-    if requestFacebook:
-        image.paste(facebookIcon, (400, 3900), mask=facebookIcon)
+    def has_transparency(img):
+        if img.mode == "P":
+            transparent = img.info.get("transparency", -1)
+            for _, index in img.getcolors():
+                if index == transparent:
+                    return True
+        elif img.mode == "RGBA":
+            extrema = img.getextrema()
+            if extrema[3][0] < 255:
+                return True
 
-    if requestInstagram:
-        image.paste(instagramIcon, (400, 4270), mask=instagramIcon)
+        return False
 
-    if requestPhone:
-        draw.text((650, 3230), requestPhone, font=infoFont)
-    if requestEmail:
-        draw.text((650, 3570), requestEmail, font=infoFont)
-    if requestFacebook:
-        requestFacebook = requestFacebook.replace("https://www.facebook.com/", "")
-        requestFacebook = requestFacebook.replace("/", "")
-        draw.text((650, 3920), requestFacebook, font=infoFont)
-    if requestInstagram:
-        requestInstagram = requestInstagram.replace("https://www.instagram.com/", "")
-        requestInstagram = requestInstagram.replace("/", "")
-        draw.text((650, 4300), requestInstagram, font=infoFont)
+    if startupPhone:
+        image.paste(phoneIcon, (400, infoCoordsList[-1]['number']), mask=phoneIcon)
+        draw.text((650, findInInfoCoordsList('phone')+30), startupPhone, font=infoFont)
 
+    if startupEmail:
+        image.paste(emailIcon, (400, infoCoordsList[-1]['number']+370), mask=emailIcon)
+        infoCoordsList.append({"id": "email", "number": infoCoordsList[-1]['number']+370})
+        draw.text((650, findInInfoCoordsList('email')+30), startupEmail, font=infoFont)
+    if startupFacebook:
+        try: 
+            if len(extractFacebookID(startupFacebook)) < 24:
+                image.paste(facebookIcon, (400, infoCoordsList[-1]['number']+370), mask=facebookIcon)
+                infoCoordsList.append({"id": "facebook", "number": infoCoordsList[-1]['number']+370})
+                startupFacebook = extractFacebookID(startupFacebook)
+                draw.text((650, findInInfoCoordsList('facebook')+30), startupFacebook, font=infoFont)
+        except:
+            pass
 
-    requestLogo = resizeImage(requestLogo, 1500)
-    if myRequest['logo'][-3:] == 'png':
-        image.paste(requestLogo, (6000, 730), mask=requestLogo)
+    if startupInstagram:
+        try:
+            if len(extractInstagramID(startupInstagram)) < 24:
+                image.paste(instagramIcon, (400, infoCoordsList[-1]['number']+370), mask=instagramIcon)
+                infoCoordsList.append({"id": "instagram", "number": infoCoordsList[-1]['number']+370})
+                startupInstagram = startupInstagram.replace("https://www.instagram.com/", "")
+                startupInstagram = startupInstagram.replace("/", "")
+                draw.text((650, findInInfoCoordsList("instagram")+30), startupInstagram, font=infoFont)
+        except:
+            pass
+    # draw.ellipse((7450, 2200, 7550, 2300), fill = 'blue', outline ='blue')
+    startupLogo = resizeImage(startupLogo, 1500)
+    startupLogoWidth, startupLogoHeight = startupLogo.size
+    logoBox = (5950, 700, 7450, 2200) #x1 y1 x2 y2
+    logoBoxHeight = logoBox[3] - logoBox[1]
+    logoPasteHeight = (730 + int((logoBoxHeight-startupLogoHeight) / 2))
+    if has_transparency(startupLogo):
+        image.paste(startupLogo, (6000, logoPasteHeight), mask=startupLogo)
     else:
-        image.paste(requestLogo, (6000, 730))
-
-    image.convert('RGBA').save(f"./pics/{myRequest['name']}_output.png")
-
+        image.paste(startupLogo, (6000, logoPasteHeight))
+    # print(f"./pics/{startup['name']}_output.png")
+    image = image.resize((int(image.size[0]/4), int(image.size[1]/4)))
+    image.convert('RGBA').save(f"./pics/{startup['name']}_output.png", optimize=True,quality=20)
     del Image
     del ImageDraw
     del ImageFont
